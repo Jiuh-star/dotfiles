be0cb299f91569ad3d25c41d244dfa4f47e9d28b
Jiuh.star <jiuh.star@gmail.com>
2024-08-17 17:05:43 +0800

    refactor(colorscheme): Add catppuccin integrations.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,15 @@ return {
     priority = 1000,
     opts = {
         transparent_background = true,
+        integrations = {
+            barbecue = {},
+            gitsigns = true,
+            mason = true,
+            cmp = true,
+            native_lsp = { enabled = true },
+            navic = { enabled = true },
+            treesitter = true,
+        }
     },
     config = function(_, opts)
         require('catppuccin').setup(opts)
