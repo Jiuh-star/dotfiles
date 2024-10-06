62b4580534b95d58e59d5bd22cae2ee42871bf5a
Jiuh.star <jiuh.star@gmail.com>
2024-10-06 12:59:52 +0800

    refactor(config): Use LazyVim.

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,14 +2,17 @@ return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
     opts = {
-        win = {
-            border = 'rounded'
-        }
+        ---@type false | "classic" | "modern" | "helix"
+        preset = "modern",
+        ---@param mapping wk.Mapping
+        filter = function(mapping)
+            return mapping.desc and mapping.desc ~= ""
+        end
     },
     keys = {
         {
             '<leader>?',
-            function ()
+            function()
                 require('which-key').show({ global = false })
             end,
             desc = 'Buffer Local Keymaps (which-key)',
